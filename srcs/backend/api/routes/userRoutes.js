82db242603ebd2d1c4ceb08e51255f08ad35e550
Álvaro Vega Gómez <alvarvg@gmail.com>
2025-04-18 11:48:45 +0200

    wip change pass

--- srcs/backend/api/routes/userRoutes.js
+++ srcs/backend/api/routes/userRoutes.js
@@ -12,6 +12,8 @@ import { getMessagesOfUser } from "../models/messageModel.js";
 import { getChatsOfUser } from "../models/chatModel.js";
 import { getMatchesOfUser } from "../models/matchModel.js";
 import { getTournamentsOfUser } from "../models/tournamentModel.js";
+import { checkCurrentPassword } from"../authUtils.js";
+import { checkNewPassword } from "../passwordReset.js";
 
 export default function createUserRoutes(fastify) {
   return [
@@ -67,6 +69,34 @@ export default function createUserRoutes(fastify) {
         if (!validateInput(req, res, [])) return;
         const user = await patchUser(req.userId, req.body);
         return res.code(200).send(user);
+      }),
+    },
+    {
+      //TODO: TEST
+      preHandler: [fastify.authenticate],
+      method: "PATCH",
+      url: "/users/password",
+      handler: asyncHandler(async (req, res) => {
+        if (
+          !validateInput(req, res, [
+            "current_password",
+            "new_password",
+            "new_password_confirm",
+          ])
+        )
+          return;
+        if (req.body.new_password !== req.body.new_password_confirm) return res.code(403).send({error:"New passwords don't match"});
+        const user = getUser(req.userId, true);
+        const isAuthorized = await checkCurrentPassword(user, req.body.current_password);
+        if (isAuthorized["error"]) return res.code(403).send(error: isAuthorized["error"]);
+        const samePassword = await checkNewPassword(user, req.body.password);
+        if (samePassword)
+          return res
+            .code(400)
+            .send({ error: "New password matches the old one" });
+        }
+        const result = await patchUser(req.userId, {password: req.body.new_password});
+        return res.code(200).send(result);
       }),
     },
     {
